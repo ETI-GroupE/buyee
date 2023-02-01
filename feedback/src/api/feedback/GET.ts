9f6e2701671608563f0b36ab6a4396490fe83e8f
NPLeeWenKang <s10203100@connect.np.edu.sg>
2023-02-01 20:27:57 +0800

    Get feedback with conditions and get length of feedback

--- feedback/src/api/feedback/GET.ts
+++ feedback/src/api/feedback/GET.ts
@@ -8,61 +8,31 @@ export default app;
 
 app.get('/feedbacks', async (req: Request, res: Response) => {
     console.log('/feedbacks (GET)')
-    // const { product_category, product_id, owner_id, product_name, product_status, limit, offset } = req.query;
+    const { product_id, user_id, limit, offset } = req.query;
 
-    // let productCategoryCondition = '';
-    // if (Array.isArray(product_category)) {
-    //     productCategoryCondition = `product_category IN (${product_category.join(", ")})`
-    // } else {
-    //     productCategoryCondition = `${product_category === undefined ? "" : `product_category = ${product_category}`}`
-    // }
+    let productIdCondition = '';
+    if (Array.isArray(product_id)) {
+        productIdCondition = `product_id IN (${product_id.join(", ")})`
+    } else {
+        productIdCondition = `${product_id === undefined ? "" : `product_id = ${product_id}`}`
+    }
 
-    // let productIdCondition = '';
-    // if (Array.isArray(product_id)) {
-    //     productIdCondition = `product_id IN (${product_id.join(", ")})`
-    // } else {
-    //     productIdCondition = `${product_id === undefined ? "" : `product_id = ${product_id}`}`
-    // }
+    let userIdCondition = '';
+    if (Array.isArray(user_id)) {
+        userIdCondition = `user_id IN (${user_id.join(", ")})`
+    } else {
+        userIdCondition = `${user_id === undefined ? "" : `user_id = ${user_id}`}`
+    }
 
-    // let ownerIdCondition = '';
-    // if (Array.isArray(owner_id)) {
-    //     ownerIdCondition = `owner_id IN (${owner_id.join(", ")})`
-    // } else {
-    //     ownerIdCondition
-    //         = `${owner_id === undefined ? "" : `owner_id = ${owner_id}`}`
-    // }
-
-    // let productNameCondition = '';
-    // if (Array.isArray(product_name)) {
-    //     product_name.forEach((pn, index) => {
-    //         product_name[index] = `'%${pn}%'`
-    //     });
-    //     productNameCondition = `product_name ILIKE ANY (array[${product_name.join(", ")}])`
-    // } else {
-    //     productNameCondition
-    //         = `${product_name === undefined ? "" : `product_name ILIKE '%${product_name}%'`}`
-    // }
-
-    // let productStatusCondition = '';
-    // if (product_status) {
-    //     productStatusCondition = `product_status = '${product_status}'`
-    // }
-
-    // const conditionString = [productCategoryCondition, productIdCondition, ownerIdCondition, productNameCondition, productStatusCondition].filter(i => i !== '')
-    // const query = {
-    //     text: `SELECT * FROM product
-    //     INNER JOIN
-    //     category ON product.product_category_id = category.category_id
-    //     ${conditionString.length ? 'WHERE' : ''} 
-    //     ${conditionString.join(" AND ")}
-    //     ORDER BY product_id ASC
-    //     LIMIT ${limit ? limit : 10}
-    //     OFFSET ${offset ? offset : 0};
-    //     `,
-    // }
+    const conditionString = [productIdCondition, userIdCondition].filter(i => i !== '')
     const query = {
         text: `
         SELECT * FROM feedback
+        ${conditionString.length ? 'WHERE' : ''} 
+        ${conditionString.join(" AND ")}
+        ORDER BY feedback_id DESC
+        LIMIT ${limit ? limit : 10}
+        OFFSET ${offset ? offset : 0};
         `,
     }
     rawDatabasePool.query(query, (err, result) => {
@@ -77,6 +47,47 @@ app.get('/feedbacks', async (req: Request, res: Response) => {
                 row.created_at_date = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
             });
             products.push(...result.rows)
+        }
+
+        res.send(products)
+    })
+
+})
+
+app.get('/feedbacks/length', async (req: Request, res: Response) => {
+    console.log('/feedbacks (GET)')
+    const { product_id, user_id } = req.query;
+
+    let productIdCondition = '';
+    if (Array.isArray(product_id)) {
+        productIdCondition = `product_id IN (${product_id.join(", ")})`
+    } else {
+        productIdCondition = `${product_id === undefined ? "" : `product_id = ${product_id}`}`
+    }
+
+    let userIdCondition = '';
+    if (Array.isArray(user_id)) {
+        userIdCondition = `user_id IN (${user_id.join(", ")})`
+    } else {
+        userIdCondition = `${user_id === undefined ? "" : `user_id = ${user_id}`}`
+    }
+
+    const conditionString = [productIdCondition, userIdCondition].filter(i => i !== '')
+    const query = {
+        text: `
+        SELECT COUNT(*) FROM feedback
+        ${conditionString.length ? 'WHERE' : ''} 
+        ${conditionString.join(" AND ")}
+        `,
+    }
+    rawDatabasePool.query(query, (err, result) => {
+        const products = {};
+        console.log(err)
+        if (err) {
+            res.status(400);
+        } else {
+            res.status(200);
+            products["count"] = result.rows[0].count
         }
 
         res.send(products)
