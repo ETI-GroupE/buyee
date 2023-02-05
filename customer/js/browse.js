71de79e362d0add7d23aa2c5022fd4484b52d286
NPLeeWenKang <s10203100@connect.np.edu.sg>
2023-02-06 02:01:09 +0800

    browser update

--- customer/js/browse.js
+++ customer/js/browse.js
@@ -7,11 +7,41 @@ const fetchProducts = () => {
                 product_name: product_name,
             },
         })
-        .then((response) => {
+        .then(async (response) => {
             const products = response.data;
             const productDisplay = document.getElementById("productDisplay");
             let productHTML = "";
-            products.forEach((product) => {
+            for (const product of products) {
+                const feedbackStats = (
+                    await axios.get(
+                        "https://buyee-feedback-ksbujg5hza-as.a.run.app/api/v1/feedbacks/stats",
+                        {
+                            params: {
+                                product_id: product.product_id,
+                            },
+                        }
+                    )
+                ).data;
+                let starHTML = "";
+                for (let i = 0; i < feedbackStats.rating; i++) {
+                    starHTML += `
+                    <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#FFD700"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                    `;
+                }
+                for (let i = 0; i < 5 - feedbackStats.rating; i++) {
+                    starHTML += `
+                    <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#808080"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                    `;
+                }
+
                 productHTML += `
                 <a href="/customer/product.html?product_id=${
                     product.product_id
@@ -32,24 +62,9 @@ const fetchProducts = () => {
                         product.product_ship_location
                     }</div>
                     <div class="productRating d-flex align-items-center">
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
-                        <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                        </svg>
+                        <span class="d-flex align-items-center">
+                            ${starHTML}
+                        </span>
                         <span class="ms-2">${
                             product.product_original_stock -
                             product.product_stock
@@ -64,9 +79,8 @@ const fetchProducts = () => {
                     } pieces left</div>
                 </a>
                 `;
-            });
+            }
             productDisplay.innerHTML = productHTML;
-            console.log(products);
         })
         .catch((error) => console.error(error));
 };
--- customer/js/product.js
+++ customer/js/product.js
@@ -38,4 +38,68 @@ const fetchProduct = () => {
         .catch((error) => console.error(error));
 };
 
+const fetchFeedback = () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const { product_id } = Object.fromEntries(urlSearchParams.entries());
+    axios
+        .get(
+            "https://buyee-feedback-ksbujg5hza-as.a.run.app/api/v1/feedbacks/stats",
+            {
+                params: {
+                    product_id: product_id,
+                },
+            }
+        )
+        .then((response) => {
+            console.log(product_id, response.data);
+            if (response.data.length === 0 || response.data.length > 1) {
+                window.location.href = "/customer/browse.html";
+            }
+            const feedbackStats = response.data;
+
+            // Rating
+            const starList = document.getElementById("starList");
+            let starHTML = "";
+            for (let i = 0; i < feedbackStats.rating; i++) {
+                starHTML += `
+                <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#FFD700"
+                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                `;
+            }
+            for (let i = 0; i < 5 - feedbackStats.rating; i++) {
+                starHTML += `
+                <svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#808080"
+                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                `;
+            }
+            starList.innerHTML = starHTML;
+
+            // Feedback
+            const feedbackList = document.getElementById("feedbackList");
+
+            let feedbackHTML = "";
+            feedbackStats.feedbacks.forEach((feedback) => {
+                feedbackHTML += `
+                <li class="list-group-item">
+                    <div style="font-size: small; font-weight: lighter;">
+                        ${feedback.date}
+                    </div>
+                    <div>
+                        ${feedback.description}
+                    </div>
+                </li>
+                `;
+            });
+            feedbackList.innerHTML = feedbackHTML;
+        })
+        .catch((error) => console.error(error));
+};
+
 fetchProduct();
+fetchFeedback();
