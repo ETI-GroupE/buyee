30d13808f79315ddaef9ca88173d536229ed1532
BrianLimMJ <S10208584@connect.np.edu.sg>
2023-02-07 13:41:43 +0800

    first commit

--- customer/js/purchase.js
+++ customer/js/purchase.js
@@ -1,24 +1,52 @@
-window.onload = function() {
-    const fetchProduct = () => {
-        axios
-            .get("https://buyee-catalog-ksbujg5hza-as.a.run.app/api/v1/products")
-            .then((response) => {
-                const products = response.data;
+const userId = sessionStorage.getItem("userId");
+fetch('https://buyee-purchase-history-1-nr7eovel5q-as.a.run.app/api/v1/allpurchase?UserID=3')
+  .then(response => {
+    console.log(response.status);
+    return response.json();
+  })
+  .then(data => {
 
-                products.forEach(product => {
-                    const productImage = document.getElementById("productImage");
-                    const productName = document.getElementById("productName");
-                    const productDescription = document.getElementById("productDescription");
+    console.log(data);
+    data.forEach(itemId => {
+    fetch('https://buyee-catalog-ksbujg5hza-as.a.run.app/api/v1/products?product_id='+ itemId.product_id)
+      .then(response => response.json())
+      .then(data => {
+        let table = document.getElementById("purchase-history-table");
+        console.log(data)
+      
+        data.forEach(item => {
+          // Create a new row element
+          let row = table.insertRow();
+    
+          // Create cells for each item property
+          let productImage = row.insertCell();
+          let productName = row.insertCell();
+          let productDescription = row.insertCell();
+          let price = row.insertCell();
+          let quantity = row.insertCell();
+          let Total = row.insertCell();
 
-                    console.log(productImage, productName, productDescription);
-
-                    productImage.src = product.image_url;
-                    productName.innerText = product.name;
-                    productDescription.innerText = product.description;
-                });
-            })
-            .catch((error) => console.error(error));
-    };
-
-    fetchProduct();
-};
+          // Set styles for each cell
+          productImage.style.textAlign = "center";
+          productName.style.textAlign = "center";
+          productDescription.style.textAlign = "center";
+          price.style.textAlign="center";
+          quantity.style.textAlign = "center";
+          Total.style.textAlign = "center";
+        
+    
+          // Populate the cells with item data
+          productImage.innerHTML = "<img src='" + item.product_image_url + "'' text-align: center width='100' height='120'>";;
+          productName.innerHTML = item.product_name;
+          productDescription.innerHTML = item.product_description;
+          price.innerHTML= "$" + item.product_price;
+          quantity.innerHTML = itemId.quantity;
+          Total.innerHTML = "$" + item.product_price;
+        });
+       
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    });
+  });
